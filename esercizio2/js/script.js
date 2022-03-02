ecea04fdf00215bbb6d822af8665017a4bf628c5
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-02 17:30:11 +0100

    implementazione html

--- esercizio2/js/script.js
+++ esercizio2/js/script.js
@@ -5,41 +5,57 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+// selezione elementi html 
+const btnGioca = document.querySelector('#gioca');
+const sceltaUser = document.querySelector('#scelta');
+const numeroSelezionato = document.querySelector('#numero');
+const h3risulato = document.querySelector('#risultato');
 
 
-let sceltaUtente = prompt('scegli pari o dispari');
 
-while (sceltaUtente != 'pari' && sceltaUtente != 'dispari') {
 
-    sceltaUtente = prompt('No, scegli pari o dispari');
+
+let sceltaUtente = sceltaUser.value;
+
+// while (sceltaUtente != 'pari' && sceltaUtente != 'dispari') {
+
+//     sceltaUtente = prompt('No, scegli pari o dispari');
+// }
+
+
+let numeroUser = numeroSelezionato.value;
+
+// while (numeroUser < 1 || numeroUser > 5) {
+//     numeroUser = parseInt(prompt('No, scrivi un numero da 1 a 5'));
+// }
+
+btnGioca.addEventListener('click', gioca);
+
+
+function gioca() {
+
+
+    let somma = 0;
+
+    let numeroComputer = Math.floor(Math.random() * 5 + 1);
+
+    console.log(numeroUser);
+    console.log(numeroComputer);
+
+
+    somma = numeroUser + numeroComputer;
+    console.log(somma);
+
+
+    if (somma % 2 == 0 && sceltaUtente == 'pari' || somma % 2 == 1 && sceltaUtente == 'dispari') {
+        h3risulato.innerHTML = 'hai vinto';
+
+    } else {
+        h3risulato.innerHTML = 'hai perso';
+    }
+
+
 }
-
-
-let numeroUser = parseInt(prompt('scrivi un numero da 1 a 5'));
-
-while (numeroUser < 1 || numeroUser > 5) {
-    numeroUser = parseInt(prompt('No, scrivi un numero da 1 a 5'));
-}
-
-let somma = 0;
-
-let numeroComputer = Math.floor(Math.random() * 5 + 1);
-
-console.log(numeroUser);
-console.log(numeroComputer);
-
-
-somma = numeroUser + numeroComputer;
-console.log(somma);
-
-
-if (somma % 2 == 0 && sceltaUtente == 'pari' || somma % 2 == 1 && sceltaUtente == 'dispari') {
-    console.log('hai vinto');
-
-} else {
-    console.log('hai perso');
-}
-
 
 
 
